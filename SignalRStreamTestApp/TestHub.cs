42eceb7b0acf8cb95886d790a5eff081adfb271b
Vesal Behrouzi <vesal.behrouzi@gmail.com>
2023-04-03 15:02:36 +0330

    add observable to stream sample

--- SignalRStreamTestApp/TestHub.cs
+++ SignalRStreamTestApp/TestHub.cs
@@ -1,17 +1,36 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading.Channels;
 
 namespace SignalRStreamTestApp
 {
     public class TestHub: Hub
     {
-        public ChannelReader<string> TestStrteam(CancellationToken cancellationToken)
+        public IAsyncEnumerable<double> TestStrteam(CancellationToken cancellationToken)
         {
-            var channel = Channel.CreateUnbounded<string>();
+            List<PriceInfo> priceInfos = new List<PriceInfo>()
+            {
+                new PriceInfo{Symbol="APPL", Price = 2300},
+                new PriceInfo{Symbol="APPL", Price = 2310},
+                new PriceInfo{Symbol="APPL", Price = 2330},
+                new PriceInfo{Symbol="APPL", Price = 2100},
+                new PriceInfo{Symbol="APPL", Price = 2200},
+                new PriceInfo{Symbol="APPL", Price = 2360},
+                new PriceInfo{Symbol="APPL", Price = 2320},
+                new PriceInfo{Symbol="APPL", Price = 2300},
+            };
 
-            _ = WriteItems(channel.Writer, 1000, cancellationToken);
 
-            return channel.Reader;
+            PriceService priceService = new PriceService();
+
+            priceService.PriceChange.ToAsyncEnumerable();
+
+            priceService.Start();
+
+            return priceService.PriceChange.StartWith(priceInfos).Select(p=>p.Price).ToAsyncEnumerable();           
+
         }
 
         private async Task WriteItems(ChannelWriter<string> writer, int delay, CancellationToken cancellationToken)
@@ -24,6 +43,72 @@ namespace SignalRStreamTestApp
             }
 
             writer.TryComplete();
+        }
+
+        
+    }
+
+    public static class ObservableExtensions
+    {
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> observable, int? maxBufferSize = null)
+        {
+            // This sample shows adapting an observable to a ChannelReader without 
+            // back pressure, if the connection is slower than the producer, memory will
+            // start to increase.
+
+            // If the channel is bounded, TryWrite will return false and effectively
+            // drop items.
+
+            // The other alternative is to use a bounded channel, and when the limit is reached
+            // block on WaitToWriteAsync. This will block a thread pool thread and isn't recommended and isn't shown here.
+            var channel = maxBufferSize != null ? Channel.CreateBounded<T>(maxBufferSize.Value) : Channel.CreateUnbounded<T>();
+
+            var disposable = observable.Subscribe(
+                                value => channel.Writer.TryWrite(value),
+                                error => channel.Writer.TryComplete(error),
+                                () => channel.Writer.TryComplete());
+
+            // Complete the subscription on the reader completing
+            channel.Reader.Completion.ContinueWith(task => disposable.Dispose());
+
+            return channel.Reader;
+        }
+    }
+
+    public class PriceService
+    {
+        private Subject<PriceInfo> priceServiceSubject;
+
+        public PriceService()
+        {
+            priceServiceSubject = new Subject<PriceInfo>();
+        }
+
+        public void Start()
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    priceServiceSubject.OnNext(new PriceInfo() { Symbol = "APPL", Price = new Random().Next(2000, 5000) });
+                    Thread.Sleep(1000);
+                }
+
+                priceServiceSubject.OnCompleted();
+            });
+        }
+
+        public IObservable<PriceInfo> PriceChange => priceServiceSubject.AsObservable();
+    }
+
+    public class PriceInfo
+    {
+        public string Symbol { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Symbol}: ${Price}";
         }
     }
 }
